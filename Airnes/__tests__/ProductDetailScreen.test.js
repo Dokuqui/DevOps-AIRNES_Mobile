0312f551db589d9951eb9eb663f17ce4a168813a
Illia <doku_isv@icloud.com>
2024-06-06 15:35:57 +0200

    test(DAP-115): Create test unitter

---
+++ Airnes/__tests__/ProductDetailScreen.test.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react-native";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import ProductDetailScreen from "../screens/ProductPageScreen";
+import { addBasketItem } from "../store/basket";
+
+jest.mock("@expo/vector-icons", () => ({
+  Ionicons: "Ionicons",
+}));
+
+jest.mock("../components/util/helper.js", () => ({
+  APIRequest: jest.fn().mockResolvedValue({
+    success: true,
+    return: {
+      ProductId: 1,
+      Name: "Product 1",
+      Description: "Lorem ipsum",
+      Price: "22.00",
+      Stock: 10,
+      Pictures: [
+        {
+          Link: "https://example.com/product1.jpg",
+        },
+      ],
+    },
+  }),
+}));
+
+jest.mock("../components/MainPage/Footer", () => () => <></>);
+
+const mockStore = configureStore([]);
+
+jest.mock("../store/basket", () => ({
+  addBasketItem: jest.fn((item) => async (dispatch) => {
+    return dispatch({
+      type: "basket/addBasketItem/fulfilled",
+      payload: item,
+    });
+  }),
+}));
+
+describe("ProductDetailScreen", () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      products: {
+        items: [],
+        status: "idle",
+        error: null,
+      },
+    });
+  });
+
+  it("renders correctly with fetched product data", async () => {
+    const { getByText, toJSON } = render(
+      <Provider store={store}>
+        <ProductDetailScreen
+          navigation={{}}
+          route={{ params: { productId: 1 } }}
+        />
+      </Provider>
+    );
+
+    await waitFor(() => {
+      expect(getByText("Product 1")).toBeTruthy();
+      expect(getByText("Lorem ipsum")).toBeTruthy();
+    });
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
